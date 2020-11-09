ae1b5eb0ec9ac31b1cc8000dfce9b3c6e68483b4
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-09 12:07:45 +0300

    feat: add system signals handler

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from loguru import logger
 from src.vk import main as run_vk_bot
 from src.telegram import run_tg_bot
 from src.utils.project_logging import get_loguru_config
+from src.utils.system_signals_handle import register_system_handlers
 
 
 def get_cli_args():
@@ -21,9 +22,9 @@ def get_cli_args():
 
 
 if __name__ == "__main__":
-    logger.configure(
-        **get_loguru_config()
-    )
+    logger.configure(**get_loguru_config())
+    register_system_handlers()
+
     bot = get_cli_args().bot
     if bot == "tg":
         run_tg_bot()
--- src/utils/system_signals_handle.py
+++ src/utils/system_signals_handle.py
@@ -4,12 +4,18 @@ from sys import platform, exit
 from loguru import logger
 
 
-def sigterm_handler(_signo, _stack_frame):
+
+def _sigterm_handler(_signo, _stack_frame):
     logger.debug("sigterm_handler executed, %s, %s" % (_signo, _stack_frame))
     exit(0)
 
 
-signal.signal(signal.SIGTERM, sigterm_handler)
-signal.signal(signal.SIGINT, sigterm_handler)
-if platform != "win32":
-    signal.signal(signal.SIGQUIT, sigterm_handler)
+def register_system_handlers():
+    """
+        Register default system handlers is necessary to gracefully stopping program in Docker
+    :return:
+    """
+    signal.signal(signal.SIGTERM, _sigterm_handler)
+    signal.signal(signal.SIGINT, _sigterm_handler)
+    if platform != "win32":
+        signal.signal(signal.SIGQUIT, _sigterm_handler)

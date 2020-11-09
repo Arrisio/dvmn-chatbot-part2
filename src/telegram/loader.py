86027b3c685d9339d4732a88bf233853f7b029e1
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-09 10:31:12 +0300

    refactor: remove comments

--- src/telegram/loader.py
+++ src/telegram/loader.py
@@ -4,10 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token=os.environ["TG_BOT_TOKEN"], parse_mode=types.ParseMode.HTML)
-# storage = RedisStorage2(
-#     host=os.getenv("REDIS_HOST", "localhost"),
-#     port=os.getenv("REDIS_PORT", 6379),
-# )
+
 storage = MemoryStorage()
 
 dp = Dispatcher(bot, storage=storage)

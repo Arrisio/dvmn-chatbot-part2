662314295455046d737d7f0535fa6f1fcad167f8
Dmitriy <dsuchkov@jet.su>
2020-11-10 09:01:43 +0300

    refactor: tg main fn

--- src/telegram/main.py
+++ src/telegram/main.py
@@ -10,7 +10,7 @@ async def on_startup(dp):
     await on_startup_notify(dp)
 
 
-def run_tg_bot():
+def main():
     logger.info("telegram service started")
     executor.start_polling(dp, on_startup=on_startup)
     logger.info("service service stopped")
@@ -22,7 +22,4 @@ if __name__ == "__main__":
     logger.configure(
         **get_loguru_config(use_default_prod_configuration=False, level="DEBUG")
     )
-
-    logger.info("service started")
-    executor.start_polling(dp, on_startup=on_startup)
-    logger.info("service stopped")
+    main()

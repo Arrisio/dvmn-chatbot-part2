fd6384d87bc1d9004c8e2374d94f4017d2ed2864
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-11 11:16:21 +0300

    refactor: rename main to specific function name

--- src/telegram/main.py
+++ src/telegram/main.py
@@ -2,24 +2,23 @@ from aiogram import executor
 from loguru import logger
 
 from src.telegram.handlers import dp
+from src.telegram.utils import on_startup_notify
+from src.utils.project_logging import get_loguru_config
 
 
 async def on_startup(dp):
-    from src.telegram.utils import on_startup_notify
-
     await on_startup_notify(dp)
 
 
-def main():
+def run_tg_bot():
     logger.info("telegram service started")
     executor.start_polling(dp, on_startup=on_startup)
     logger.info("service service stopped")
 
 
 if __name__ == "__main__":
-    from src.utils.project_logging import get_loguru_config
 
     logger.configure(
         **get_loguru_config(use_default_prod_configuration=False, level="DEBUG")
     )
-    main()
+    run_tg_bot()

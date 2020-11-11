b16ba1826c5903f2effd491ea4516fd89ab21dcf
Dmitriy Suchkov <dsuchkov@jet.su>
2020-11-11 11:17:59 +0300

    fix: env var name

--- src/telegram/utils.py
+++ src/telegram/utils.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 
 async def on_startup_notify(dp: Dispatcher):
-    if admin := os.getenv("TL_ADMIN_ID"):
+    if admin := os.getenv("TG_ADMIN_ID"):
         try:
             await dp.bot.send_message(admin, "Бот Запущен и готов к работе!")
         except Exception as err:
